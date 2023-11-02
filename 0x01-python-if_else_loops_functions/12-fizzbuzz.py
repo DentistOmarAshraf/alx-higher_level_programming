798145b6dd1d1ff6a9675219a7e50bdfc4bc7379
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-02 23:17:34 +0000

    ---

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,12 @@ def fizzbuzz():
     for i in range(100):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
+            continue
         elif i % 3 == 0:
             print("Fizz", end=" ")
+            continue
         elif i % 5 == 0:
             print("Buzz", end=" ")
+            continue
         else:
             print("{:d}".format(i), end=" ")
