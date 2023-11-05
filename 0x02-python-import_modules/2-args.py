c59a1967c308b83d9b8642ea4cb8a8ed30ea38b7
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-05 11:10:22 +0000

    ----

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     ac = len(argv)
-    print("{:d} argument:".format(ac - 1))
-    if ac > 1:
-        for i in range(1, ac):
-            print("{:d}: {:s}".format(i, argv[i]))
+    if ac - 1 == 0:
+        print("{:d} arguments.".format(ac - 1))
+    elif ac - 1 == 1:
+        print("{:d} argument:".format(ac - 1))
+    else:
+        print("{:d} arguments:".format(ac - 1))
+    for i in range(1, ac):
+        print("{:d}: {:s}".format(i, argv[i]))
