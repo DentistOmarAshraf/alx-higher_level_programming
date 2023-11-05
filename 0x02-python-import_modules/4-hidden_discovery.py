b735df248c9ccf9b0bf1302c697e99ea098ef3db
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-05 12:29:25 +0000

    i forgat about __name__ == main

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-names = dir(hidden_4)
-for name in names:
-    if name[0] != '_':
-        print("{:s}".format(name))
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[0:2] != "__":
+            print("{:s}".format(name))
