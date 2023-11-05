33431b26cb8c491118d9236be0e1bad6c1be084a
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-05 11:49:36 +0000

    I still affected by c

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     ac = len(argv)
-    if ac == 1:
-        print("{:d}".format(0))
-    else:
-        res = 0
-        for i in range(1, ac):
-            res += int(argv[i])
-        print("{:s}".format(str(res)))
+    res = 0
+    for i in range(1, ac):
+        res += int(argv[i])
+    print("{:d}".format(res))
