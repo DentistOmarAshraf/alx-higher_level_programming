ebacbd1bbaa9ca95549b187128f4c15f616b44fc
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-06 19:00:55 +0000

    1st - dlt char

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        new = ""
+        for let in my_string:
+            if let == "c" or let == "C":
+                continue
+            new += let
+        return new
