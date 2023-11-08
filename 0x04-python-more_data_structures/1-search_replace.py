3df0cb867f8483e3999f480c46b7f29cd9af2f08
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-08 22:23:22 +0000

    1st - search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    while(True):
+        try:
+            idx = new_list.index(search)
+            new_list[idx] = replace
+        except ValueError:
+            break
+    return new_list
