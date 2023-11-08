f123256e4f44a8a1381dd5cf00f90b3d0cf1c2ba
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-08 19:24:39 +0000

    I feel somtime like I am stupid

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[[]]):
     new = []
-    x = []
     for i in range(len(matrix)):
+        x = []
         for j in range(len(matrix[i])):
             x.append(matrix[i][j] ** 2)
         new.append(x)
-        x = []
     return new
