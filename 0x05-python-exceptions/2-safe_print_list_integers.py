2477fd7347475923a493a1e6ef8a8f53c02f59ac
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-20 12:59:01 +0000

    ---

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             pr += 1
-        except (TypeError, IndexError, ValueError):
+        except (IndexError, ValueError):
             pass
         i += 1
     print("")
