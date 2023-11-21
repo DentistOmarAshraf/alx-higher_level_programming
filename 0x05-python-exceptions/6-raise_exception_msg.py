1952cffa011cdc01de2f4f9d2d3f62e4ba8be070
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-21 11:02:26 +0000

    -----

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def raise_exception(msg=""):
-    if msh == None or len(msg) = 0:
-        raise TypeError()
-    raise TypeError(msg)
+    raise NameError(msg)
+
+try:
+    raise_exception("C is fun")
+except NameError as ne:
+    print(ne)
