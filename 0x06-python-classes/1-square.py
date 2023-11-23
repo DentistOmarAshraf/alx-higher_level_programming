6180cefed52d195728511fba10a5efb29a4215d2
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-23 02:15:52 +0000

    ----

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-
 """
 Again Its about square
 """
 
 
 class Square:
+
     """Keep it Quite"""
-    __size = None
+
+    __size = 0
+    def __init__(self, size=0):
+
+        """Constractor"""
+
+        self._Square__size = size
+        Square.__size += size
