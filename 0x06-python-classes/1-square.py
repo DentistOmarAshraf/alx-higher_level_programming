c4919319a69661ef5566b9fd46a3f3f0df607029
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-23 02:23:49 +0000

    ---

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,8 +8,17 @@ class Square:
     """Keep it Quite"""
 
     __size = 0
-    def __init__(self, size=0):
 
-        """Constractor function"""
+    def __init__(self, size=0):
+        """Constractor function
+
+        Args:
+            param1 (self): instance
+            param2 (int): size integer
+
+        Returns:
+            None: it just constract
+        """
+
         self._Square__size = size
         Square.__size += size
