8f54d7f85c50738be78fb80a88f64a664e2847f6
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-28 02:57:25 +0000

    --------

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,6 +78,10 @@ class Rectangle:
         Return:
             instance
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         return (cls(size, size))
 
     @staticmethod
