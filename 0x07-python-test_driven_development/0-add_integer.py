315656c1704a24379fec5a88c99f45eecda62017
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-28 17:39:37 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+This is Add integer fucntion module
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    fucntion to add two integer
+    """
+    
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    return int(a + b)
