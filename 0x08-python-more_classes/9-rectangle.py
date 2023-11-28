825cb670d4fe3ef87f8cf29ce0dfd511ff4f86c1
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-28 03:50:47 +0000

    fuck u ALX

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -79,9 +79,9 @@ class Rectangle:
             instance
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         return (cls(size, size))
 
     @staticmethod
