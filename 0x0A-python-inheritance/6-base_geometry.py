80d44c5f79e2b1b7201c285b1fbd744c088bdbb1
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 11:17:23 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Empty Class
+"""
+
+
+class BaseGeometry:
+    """
+    Just Empty class that have one method to raise Error
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
