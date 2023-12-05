19e74c98e47599ab580f44d825002dd9310d0264
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 10:21:51 +0000

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function to know if Drived class is subclass of base
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function to know drived class
+    """
+    return isinstance(obj, a_class)
