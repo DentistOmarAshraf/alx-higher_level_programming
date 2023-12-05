4ae53767026cf9b548aecb0e5c6dfde68777f82e
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 23:27:38 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function to Read Module
+"""
+
+
+def read_file(filename=""):
+    """
+    Funciton to read from file
+    Args:
+        param1(filename): file name
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        data = f.read()
