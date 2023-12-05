ddbfc4d5ed3792929d8d5c38faafd76b0ec3aee2
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 10:34:03 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function to detrmine isinstance of class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    return the (isinstance())
+    """
+    return isinstance(obj, a_class)
