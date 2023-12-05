ed64e90cd7f1ccc4874303a1f9b590d1dea9fcf8
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 17:54:00 +0000

    ----

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,11 +8,8 @@ function add attribute to instance
 >>> mc = MY()
 >>> mc.name = "omar"
 >>> add_attribute(mc, "name", "Jhon")
-Traceback (most recent call last):
-    ...
-TypeError: can\'t add new attribute
 >>> print(mc.name)
-omar
+Jhon
 >>> a = "omar"
 >>> add_attribute(a, "name", "Bob")
 Traceback (most recent call last):
@@ -28,9 +25,6 @@ TypeError: can\'t add new attribute
 
 def add_attribute(obj, attribute, value):
     """fucntion to add attribute to object if itsnot a built in"""
-    builtin = [int, float, complex, list, tuple, range, str, bytes]
-    builtin2 = [bytearray, memoryview, set, frozenset, dict, bool]
-    builtin = builtin + builtin2
-    if type(obj) in builtin or hasattr(obj, attribute):
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
