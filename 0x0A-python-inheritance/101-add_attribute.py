9b9cb965efe7f086e2ded00ee0055face9a0a998
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-05 17:41:13 +0000

    ----

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -25,7 +25,8 @@ TypeError: can\'t add new attribute
 def add_attribute(obj, attribute, value):
     """fucntion to add attribute to object if itsnot a built in"""
     builtin = [int, float, complex, list, tuple, range, str, bytes]
-    builtin.append([bytearray, memoryview, set, frozenset, dict, bool])
-    if type(obj) in builtin or obj == None:
+    builtin2 = [bytearray, memoryview, set, frozenset, dict, bool]
+    builtin = builtin + builtin2
+    if type(obj) in builtin:
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
