30303311323f675dacffee4b3ce9c1870de43a0c
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-06 01:52:44 +0000

    ---

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ Fucntion to save Json to file
 def save_to_json_file(my_obj, filename):
     """function to save Json to file"""
     import json
-    with open(filename, "a+", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
