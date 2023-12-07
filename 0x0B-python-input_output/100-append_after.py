b0f298d0554c133baca784fac389bba2efb19265
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-07 04:32:21 +0000

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,10 +14,12 @@ def append_after(filename="", search_str="", new_str=""):
             line = f.readline()
             if line != '':
                 arr.append(line)
+    ne = []
     for i in range(len(arr)):
+        ne.append(arr[i])
         if search_str in arr[i]:
-            arr.insert(i + 1, new_str)
+            ne.append(new_str)
 
     with open(filename, "w", encoding="utf-8") as f:
-        for i in arr:
+        for i in ne:
             f.write(i)
