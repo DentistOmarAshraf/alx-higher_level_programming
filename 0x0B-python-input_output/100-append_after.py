ce97e978dcc7ec647bec65b27bf1af6bab6728ac
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-09 09:27:26 +0000

    100-append_after.py update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,12 +8,9 @@ def append_after(filename="", search_str="", new_str=""):
     """Function to append text After certin txt"""
     arr = []
     with open(filename, "r", encoding="utf-8") as f:
-        line = f.readline()
-        arr.append(line)
-        while line:
-            line = f.readline()
-            if line != '':
-                arr.append(line)
+        for line in f:
+            arr.append(line)
+
     ne = []
     for i in range(len(arr)):
         ne.append(arr[i])
