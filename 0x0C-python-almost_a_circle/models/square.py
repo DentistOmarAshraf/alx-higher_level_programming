8db654977affe38ef81a8ac7371e1565b1156348
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-11 22:05:03 +0000

    Task 11 Square Size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor Function"""
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        super().width_height_validator('width', size)
+        self.__size = size
 
     def __str__(self):
         """String represatation"""
         string = "[Square] ({}) ".format(self.id)
-        string += "{}/{} - {}".format(self.x, self.y, self.width)
+        string += "{}/{} - {}".format(self.x, self.y, self.size)
         return string
