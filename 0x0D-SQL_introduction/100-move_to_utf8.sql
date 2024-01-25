2720a3bd10f75c7f06c749fbc37dba39713e3f78
DentistOmarAshraf <omar_ashraf@live.com>
2024-01-25 17:21:57 +0000

    ----

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- convert data base to UTF8
-ALTER TABLE second_table  CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table  CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
