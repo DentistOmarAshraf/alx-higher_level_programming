1c1db1bb1698cf35a3d27db25a41e9ba571b43c3
DentistOmarAshraf <omar_ashraf@live.com>
2024-01-26 08:55:14 +0000

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,5 +3,5 @@ ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
-CHANGE name name VARCHAR(191) CHARACTER
+CHANGE name name VARCHAR(256) CHARACTER
 SET utf8mb4 COLLATE utf8mb4_unicode_ci;
