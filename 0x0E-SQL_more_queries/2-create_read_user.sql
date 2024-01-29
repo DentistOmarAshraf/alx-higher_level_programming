4da5c63faf9e5034dcc120e315c560c1745767a7
DentistOmarAshraf <omar_ashraf@live.com>
2024-01-29 15:41:10 +0000

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Create Database & new User & grant select privilege
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS
+'user_0d_2'@'localhost'
+IDENTIFIED WITH mysql_native_password BY
+'user_0d_2_pwd';
+FLUSH PRIVILEGES;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+WITH GRANT OPTION;
