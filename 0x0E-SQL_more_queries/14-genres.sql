2746a0782f921d7c33b6081602de2244d69fa7b3
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-02 09:50:30 +0000

    14-genres.sql

---
+++ 0x0E-SQL_more_queries/14-genres.sql
@@ -0,0 +1,6 @@
+-- Find Genre of movie
+SELECT tg.name AS name
+FROM tv_shows sh
+INNER JOIN tv_show_genres gen ON sh.id=gen.show_id
+INNER JOIN tv_genres tg ON gen.genre_id=tg.id
+WHERE sh.title='Dexter';
