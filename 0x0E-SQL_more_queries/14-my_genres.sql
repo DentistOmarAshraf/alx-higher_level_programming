8f817a15514a1921b9ac89b202573074a58c0223
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-02 09:56:50 +0000

    ---

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,4 @@ SELECT tg.name AS name
 FROM tv_shows sh
 INNER JOIN tv_show_genres gen ON sh.id=gen.show_id
 INNER JOIN tv_genres tg ON gen.genre_id=tg.id
-WHERE sh.title='Dexter';
+WHERE sh.title='Dexter' ORDER BY tg.name ASC;
