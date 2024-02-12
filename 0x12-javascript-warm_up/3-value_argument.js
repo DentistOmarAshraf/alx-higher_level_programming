5ee6e216db7eea68b81b8f959637a6e342726977
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 19:28:44 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -10,9 +10,5 @@ const hasarg = args.some((value, index) => index > 1);
 if (!hasarg) {
   console.log('No argument');
 } else {
-  args.forEach((val, ind) => {
-    if (ind > 1) {
-      console.log(val);
-    }
-  });
+  console.log(args[2]);
 }
