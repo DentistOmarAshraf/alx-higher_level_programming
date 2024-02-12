34cfca5296763f008d6df7658114d61f3e7f4c15
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 22:21:25 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -16,6 +16,14 @@ args.forEach((value, index) => {
 if (arr.length <= 1) {
   console.log(0);
 } else {
-  arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
   console.log(arr[arr.length - 2]);
 }
