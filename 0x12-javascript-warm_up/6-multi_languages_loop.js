3a5157eaa804f6a384b8635d7dcdffeb498105e1
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 19:47:26 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -7,7 +7,7 @@
 const stats = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
-while (i < stats.length){
-	console.log(stats[i]);
-	i++;
+while (i < stats.length) {
+  console.log(stats[i]);
+  i++;
 }
