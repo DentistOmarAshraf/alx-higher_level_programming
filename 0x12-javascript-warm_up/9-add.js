9808f6c70c1a937c22f942d7a794947f13d3c59f
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 20:14:13 +0000

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,8 +6,8 @@
 
 const args = process.argv;
 
-function add(a, b){
-	console.log(parseInt(a) + parseInt(b));
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
 }
 
 add(args[2], args[3]);
