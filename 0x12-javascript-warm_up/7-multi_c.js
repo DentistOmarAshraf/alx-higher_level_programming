64ee7c60126323ea613864c8c89c22ddef3baf7f
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 19:57:16 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+ * print (x)time from argv
+ */
+
+const args = process.argv;
+const times = parseInt(args[2]);
+
+if (times) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
