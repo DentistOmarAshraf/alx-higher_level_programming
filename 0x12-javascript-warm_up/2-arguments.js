975a0b827f5f5e6af298e6eb44af50c4d86ff2df
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 16:46:37 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 
+/*
+ * Printing args
+ */
+
 const argv = process.argv;
 
-if (argv.length === 2) {
+if (argv.length <= 2) {
   console.log('No argument');
-} else {
+} else if (argv.length === 3) {
   console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 }
