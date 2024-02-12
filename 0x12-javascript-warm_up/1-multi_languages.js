e5387d3f9ef92bdf29b0437e25d8200cdcb2a360
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-12 14:13:02 +0000

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+ * Printnig script
+ */
+
+const lineOne = 'C is fun';
+const lineTow = 'Python is cool';
+const lineThree = 'JavaScript is amazing';
+
+console.log(`${lineOne}\n${lineTow}\n${lineThree}`);
