bfc4f723b214caebf06b4124d38fb89bf799e419
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-17 08:51:18 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
                            host="localhost",
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    search = "SELECT id, name"
+    search += " FROM states where name=\"{}\"".format(sys.argv[4])
+    cur.execute(search)
     q_row = cur.fetchall()
 
     for row in q_row:
