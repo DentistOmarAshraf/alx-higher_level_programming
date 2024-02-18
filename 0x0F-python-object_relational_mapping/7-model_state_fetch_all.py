a8d296b63cfa171d64daf5e2559fde01dfdb6111
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-18 23:31:07 +0000

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
+"""Usr and password and database"""
 usr = sys.argv[1]
 pd = sys.argv[2]
 db = sys.argv[3]
@@ -18,6 +18,6 @@ engine = create_engine(eng_url, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-for obj in session.query(State).all():
+"""SELECT id, name FROM states ORDER BY id"""
+for obj in session.query(State).order_by(State.id).all():
     print(f"{obj.id}: {obj.name}")
