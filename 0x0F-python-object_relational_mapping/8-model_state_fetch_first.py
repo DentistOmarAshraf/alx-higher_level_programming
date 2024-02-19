0ddc3f60dac2f5d182df55718b8a111f1a9eb8a3
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-19 10:11:16 +0000

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Fetch First state
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+"""creating Engine"""
+engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+"""Using query.filter to fetch first state"""
+
+all_obj = session.query(State).filter(State.id == '1').all()
+
+if (len(all_obj) == 0):
+    print("Nothing")
+    exit(0)
+else:
+    for obj in all_obj:
+        print(f"{obj.id}: {obj.name}")
