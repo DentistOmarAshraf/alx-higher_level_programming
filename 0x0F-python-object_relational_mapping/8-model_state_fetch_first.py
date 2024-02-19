20e4bbd2c2c0fe20857518bdca8d06fb3305174b
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-19 10:14:57 +0000

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,20 +7,20 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-"""creating Engine"""
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+if __name__ == "__main__":
+    """creating Engine"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                 sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-"""Using query.filter to fetch first state"""
+    """Using query.filter to fetch first state"""
 
-all_obj = session.query(State).filter(State.id == '1').all()
+    all_obj = session.query(State).filter(State.id == '1').all()
 
-if (len(all_obj) == 0):
-    print("Nothing")
-    exit(0)
-else:
-    for obj in all_obj:
-        print(f"{obj.id}: {obj.name}")
+    if (len(all_obj) == 0):
+        print("Nothing")
+    else:
+        for obj in all_obj:
+            print(f"{obj.id}: {obj.name}")
