c907da5d40d9109e57fe696bfa8c8e6089c4aae1
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-23 21:50:16 +0000

    pycode

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,18 +9,22 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
+    """Engine Start"""
     usr = sys.argv[1]
     pd = sys.argv[2]
     db = sys.argv[3]
     engine = create_engine(f"mysql+mysqldb://{usr}:{pd}@localhost:3306/{db}")
+    """Loading tables"""
     Base.metadata.create_all(engine)
 
+    """Session Start"""
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state_list = session.query(State).order_by(State.id).all()
     for state in state_list:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("\t{}:{}".format(city.id, city.name))
+
     session.close()
