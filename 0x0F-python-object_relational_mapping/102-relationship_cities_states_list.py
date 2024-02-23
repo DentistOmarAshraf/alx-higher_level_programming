04abc06f4aa5b609593da11099be1e76d740b090
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-23 22:56:18 +0000

    102-relationship_cities_states_list.py

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+print list from relateion on-to-many
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    """Engine Start"""
+    usr = sys.argv[1]
+    pd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{usr}:{pd}@localhost:3306/{db}",
+                           pool_pre_ping=True)
+
+    """Session Start"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        for ci in state.cities:
+            print(f"{ci.id}: {ci.name} -> {state.name}")
