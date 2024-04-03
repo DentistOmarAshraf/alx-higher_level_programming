0f2db1843608e6aebd60b7a046d917299d19ec2d
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-03 13:28:55 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request
-curl -H 'email:test@gmail.com' -H 'subject:I will always be here for PLD' -sX POST "$1"
+curl --data 'email=test@gmail.com&subject=I will always be here for PLD' -sX POST "$1"
