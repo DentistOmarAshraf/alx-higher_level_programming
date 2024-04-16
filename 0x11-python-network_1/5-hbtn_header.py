dc49f62b3ea4e9bcbb5886f739f3fd12e84376f4
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-16 12:42:30 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,14 @@ Fetching Using requests lib
 
 
 if __name__ == "__main__":
+    """
+    Fetchin using requests lib
+    """
     import sys
     import requests
 
     res = requests.get(sys.argv[1])
-    print(res.headers["X-Request-Id"])
+    try:
+        print(res.headers["X-Request-Id"])
+    except KeyError as e:
+        pass
