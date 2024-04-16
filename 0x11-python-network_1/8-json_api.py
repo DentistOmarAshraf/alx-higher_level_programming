a902efacaa3cb9d9b95cb3be6da736e9632afba1
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-16 14:06:11 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,12 +10,20 @@ if __name__ == "__main__":
     """
     import sys
     import requests
+    from simplejson.errors import JSONDecodeError
     
     if len(sys.argv) > 1:
         srch = sys.argv[1]
     else:
         srch = ""
-    url = "http://0.0.0.0:5000/search_user"
+    url = "http://0.0.0.0/search_user"
     dt = {"q": srch}
     res = requests.post(url, data=dt)
-    print(res.text)
+    try:
+        data = res.json()
+        if "id" in data.keys():
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except JSONDecodeError as e:
+        print("Not a valid JSON")
