4fda31a5b2a620fe273dec79e57b2a4e25b68fd9
DentistOmarAshraf <omar_ashraf@live.com>
2024-04-18 04:10:41 +0000

    Nice OMAR

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+list GitHub Repo Commits
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    passwd = ""
+    head = {"Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {passwd}",
+            "X-GitHub-Api-Version": "2022-11-28"}
+
+    url = f"http://api.github.com/repos/{owner}/{repo}/commits"
+
+    res = requests.get(url, head)
+    data = res.json()
+
+    y = 0
+    for x in data:
+        print("{}: {}".format(x["sha"], x["author"]["login"]))
+        y += 1
+        if y == 10:
+            break
