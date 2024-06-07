112a7b6160054ce80c2465e9d95cd70b4070c3fc
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-07 22:34:20 +0000

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Using Request Module function
+ */
+
+const request = require('request');
+const args = process.argv.slice(2);
+
+request(args[0], (err, response, body) => {
+  console.log(response.statusCode);
+
+  if (err) {
+    console.error(err);
+  }
+});
